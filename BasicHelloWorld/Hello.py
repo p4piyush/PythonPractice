dc44a29f7f53979f6a3fe96d223bb830d480c911
P4Piyush <piyush.max2@gmail.com>
2022-06-19 21:09:15 +0530

    first commit

---
+++ BasicHelloWorld/Hello.py
@@ -0,0 +1,12 @@
+def HelloFun(name="User"): #This function accept string value and also have Deafault value if not pass
+    """
+        THIS FUNCTION ACCEPT STRING AS ARGUMENT AND PRINT IT 
+        IF NO STRING PROVIDED THEN IT WILL PRINT DEFAULT VALUE
+        THIS FUCTION WILL PRINT ONLY NO RETURN VALUES
+    """
+    print(f"{name} Welcome to Python \n")
+
+
+
+HelloFun()
+HelloFun("Piyush")
